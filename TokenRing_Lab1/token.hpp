e84dac36dae4193ba0f90ed22c78a6fb47a31f95
kjano10 <kuba.jano3@gmail.com>
2019-05-18 23:12:11 +0200

    Remove duplicate

--- TokenRing_Lab1/token.hpp
+++
@@ -1,9 +0,0 @@
-#define BUFFER_SIZE 1024
-
-typedef struct{
-    int type; //1 - MSG, 2 - CONNECT, 3 - ADD, 4 - EMPTY
-    char msg[BUFFER_SIZE];
-    int portD;
-    int portS;
-    char ipAddr[BUFFER_SIZE];
-} token;
